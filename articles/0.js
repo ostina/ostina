b17c032b5abd50203014f936c034ed093581310d
ostina <83616519+ostina@users.noreply.github.com>
2021-06-23 19:51:07 +0200

    Update 0.js

--- articles/0.js
+++ articles/0.js
@@ -5,13 +5,14 @@ _=`
   dettati alla veggente Silvana Orlandi durante le apparizioni.
   Le apparizioni avvengono l'ultima domenica di ogni mese pari sin dal 10 luglio del 1993. <br>
   Leggi e medita le esortazioni della Madonna e capirai cosa va o non va nella tua vita;
-  sentirai la sua voce che parla al tuo cuore, ti invita alla preghiera e ti porta Gesù. <br>
-  <br>
+  sentirai la sua voce che parla al tuo cuore, ti invita alla preghiera e ti porta Gesù.
+</p>
+<img id="madonna" src="res/madonna.jpg" alt="Madonna della Riconciliazione">
+<p>
   Il codice sorgente di questo sito è aperto e visitabile su GitHub. <br>
   Se si vuole riportare un problema è possibile aprire una nuova issue
   con possibilmente una soluzione. <br>
   Se si vuole contribuire al sito è possibile creare una pull-request.
 </p>
-<img id="madonna" src="res/madonna.jpg" alt="Madonna della Riconciliazione">
 
 `
