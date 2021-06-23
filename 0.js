7c00ee56acc60df8bafc8a9a82e6fad9068caa69
ostina <83616519+ostina@users.noreply.github.com>
2021-06-23 19:39:12 +0200

    Add files via upload

---
+++ 0.js
@@ -0,0 +1,17 @@
+_=`
+
+<p>
+  In questo sito troverai i messaggi della Madonna della Riconciliazione di Ostina (Reggello, FI)
+  dettati alla veggente Silvana Orlandi durante le apparizioni.
+  Le apparizioni avvengono l'ultima domenica di ogni mese pari sin dal 10 luglio del 1993. <br>
+  Leggi e medita le esortazioni della Madonna e capirai cosa va o non va nella tua vita;
+  sentirai la sua voce che parla al tuo cuore, ti invita alla preghiera e ti porta Gesù. <br>
+  <br>
+  Il codice sorgente di questo sito è aperto e visitabile su GitHub. <br>
+  Se si vuole riportare un problema è possibile aprire una nuova issue
+  con possibilmente una soluzione. <br>
+  Se si vuole contribuire al sito è possibile creare una pull-request.
+</p>
+<img id="madonna" src="res/madonna.jpg" alt="Madonna della Riconciliazione">
+
+`
