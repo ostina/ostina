a92f220235a6f2f1e26aba9306e2b0fabe448a02
ostina <ostina@users.noreply.github.com>
2021-08-31 02:01:02 +0200

    changed the articles fetching system

--- js/main.js
+++ js/main.js
@@ -11,50 +11,29 @@ const extras =
   168: '28° anniversario'
 }
 
-onload = () =>
-(
-  onhashchange = () =>
-  {
-    if (!minWidth576px.matches)
-      menu.checked = false
-
-    try
-    {
-      article.innerHTML = eval('_' + location.hash.substr(1))
-    }
-    catch
-    {
-      article.textContent = articleError
-    }
-  }
-)()
-
-addArticle(0)
-
-for (let i = 1; i <= navCount; i++)
+for (let i = 1; i <= navCount; ++i)
 {
-  addArticle(i)
   addNavLink(i, i + navSuffix)
 
   if (i in extras)
-  {
-    addArticle('e' + i)
     addNavLink('e' + i, extras[i])
-  }
 }
+
+;(onhashchange = () =>
+{
+  fetch(`articles/${location.hash.slice(1) || 0}.html`)
+    .catch(() => location.reload())
+    .then(r => r.ok ? r.text() : articleError)
+    .then(s => article.innerHTML = s)
+
+  if (!minWidth576px.matches)
+    menu.checked = false
+})()
 
 function addNavLink(id, text)
 {
-  const a = document.createElement('a')
-  a.id = id
-  a.href = '#' + id
-  a.textContent = text
-  nav.prepend(a)
-}
-
-function addArticle(id)
-{
-  const script = document.createElement('script')
-  script.src = 'articles/' + id + '.js'
-  document.body.append(script)
+  nav.insertAdjacentHTML(
+    'afterbegin',
+    /*html*/ `<a id="${id}" href="#${id}">${text}</a>`
+  )
 }
