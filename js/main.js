6dcfbede34811c094243f1e5c48c45a937917749
ostina <ostina@users.noreply.github.com>
2022-11-19 12:43:08 +0100

    new latest alias feature (#ultimo)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const articleError = article.getAttribute('data-error')
 const nav = document.querySelector('nav')
 const navCount = parseInt(nav.getAttribute('data-count'))
 const navSuffix = nav.getAttribute('data-suffix')
+const navLatestAlias = nav.getAttribute('data-latestalias')
 const menu = document.querySelector('#menu')
 const root = document.querySelector(':root')
 const minWidth576px = window.matchMedia('(min-width: 576px)')
@@ -25,13 +26,23 @@ for (let i = 1; i <= navCount; ++i)
 
 ;(onhashchange = () =>
 {
-  fetch(`articles/${location.hash.slice(1) || 0}.html`)
-    .catch(() => location.reload())
-    .then(r => r.ok ? r.text() : articleError)
-    .then(s => article.innerHTML = s)
+  let articleName = location.hash.slice(1).toLowerCase() || 0
 
-  if (!minWidth576px.matches)
-    menu.checked = false
+  switch (articleName)
+  {
+    case navLatestAlias:
+      location = nav.firstChild.href
+      return;
+
+    default:
+      fetch(`articles/${articleName}.html`)
+        .catch(() => location.reload())
+        .then(r => r.ok ? r.text() : articleError)
+        .then(s => article.innerHTML = s)
+
+      if (!minWidth576px.matches)
+        menu.checked = false
+  }
 })()
 
 function addNavLink(id, text)
